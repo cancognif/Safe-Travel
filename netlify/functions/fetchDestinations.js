7d2f9770abc78ad13b9ce12855be8768a55a5e32
cancognif <cancognif.ai@gmail.com>
2025-11-21 15:25:47 +0100

    Update fetchDestinations.js

--- netlify/functions/fetchDestinations.js
+++ netlify/functions/fetchDestinations.js
@@ -2,17 +2,11 @@ exports.handler = async (event, context) => {
   try {
     const { origin } = JSON.parse(event.body);
 
-    console.log("📌 Fetching latest prices for origin:", origin);
-
     const url =
       `https://api.travelpayouts.com/v2/prices/latest?` +
-      `origin=${origin}&currency=EUR&limit=50&show_to_affiliates=false&sorting=price&token=02dd565a82ec75665c68543e34abc5d6`;
-
-    console.log("🔗 Calling Travelpayouts URL:", url);
+      `origin=${origin}&currency=EUR&limit=100&show_to_affiliates=false&sorting=price&token=02dd565a82ec75665c68543e34abc5d6`;
 
     const res = await fetch(url);
-    console.log("📨 Travelpayouts status:", res.status);
-
     const data = await res.json();
 
     return {
