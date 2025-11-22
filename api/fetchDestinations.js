ac92edd422fd5c0b0d144c8dae0fef68b0a93931
cancognif <cancognif.ai@gmail.com>
2025-11-22 13:42:20 +0100

    Update fetchDestinations.js

--- api/fetchDestinations.js
+++ api/fetchDestinations.js
@@ -1,1 +1,22 @@
+export default async function handler(req, res) {
+  try {
+    const body = JSON.parse(req.body || "{}");
+    const origin = body.origin || "MIL";
+
+    const url =
+      `https://api.travelpayouts.com/v2/prices/latest?` +
+      `origin=${origin}&currency=EUR&limit=100&show_to_affiliates=false&sorting=price&token=02dd565a82ec75665c68543e34abc5d6`;
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    res.status(200).json(data);
+
+  } catch (e) {
+    res.status(500).json({
+      error: "Internal API error",
+      details: e.message
+    });
+  }
+}
 
